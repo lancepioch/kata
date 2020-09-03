7911f45dab73bb8d3936bb48feea63e63ddd4bb0
Lance Pioch <git@lance.sh>
2020-09-03 16:28:12 -0400

    Add the api routes to the root namespace

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('quotes/quotd', 'QuotesController@daily');
+Route::get('quotes/random', 'QuotesController@random');
+Route::get('quotes/search', 'QuotesController@search');
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
