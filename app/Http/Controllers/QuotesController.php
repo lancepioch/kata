d923c91b6636871a873a625e76d5a1e2f117c910
Lance Pioch <git@lance.sh>
2020-09-03 16:27:52 -0400

    Create a backend api service to consume the quote manager class

---
+++ app/Http/Controllers/QuotesController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Quotes;
+use Illuminate\Http\Request;
+
+class QuotesController extends Controller
+{
+    public function daily(Request $request)
+    {
+        $date = $request->d ?? now();
+
+        return (new Quotes())->quotd($date);
+    }
+
+    public function random(Request $request)
+    {
+        return (new Quotes())->random();
+    }
+
+    public function search(Request $request)
+    {
+        $search = $request->q;
+
+        abort_unless(strlen($search) > 0, 400, 'You must provide some input to the search!');
+
+        return (new Quotes())->search($search);
+    }
+}
